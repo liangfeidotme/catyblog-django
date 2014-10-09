051f873073319f4a6231d141516cdd2b32e2e784
liangfei <njliangfei@gmail.com>
2014-10-09 16:23:43 +0800

    add catykanji

--- catyblog/views.py
+++ catyblog/views.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 __author__ = 'lyndon'
 
 from django.shortcuts import render
+from django.http import HttpResponse
 from blog.views import fill_page_with
 
+import json
 
 def about(request):
     return render(request, 'about.html',
@@ -12,3 +15,12 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html',
                   fill_page_with(nav_name='contact'))
+
+def catykanji_contents(request):
+    if request.method == 'GET':
+        contents = {}
+        contents['character'] = '鬱'
+        contents['pronunciation'] = 'うつ'
+        contents['phrases'] = '鬱病'
+
+        return HttpResponse(json.dumps(contents), content_type='application/json')
